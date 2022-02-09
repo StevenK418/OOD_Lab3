d0e00fbd89dfbb34327cd76ee1a231a5e78d6c80
Steven Kelly <stevenkelly418@gmail.com>
2022-02-09 10:48:20 +0000

    => Added exercise 2 to the repo.

---
+++ Lab3Ex1/Lab3Ex2/MyFileInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3Ex2
+{
+    class MyFileInfo
+    {
+        public string Name { get; set; }
+        public long Length { get; set; }
+        public DateTime CreationTime { get; set; }
+    }
+}
---
+++ Lab3Ex1/Lab3Ex2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab3Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Call the Query Syntax Method
+            UsingQuerySyntax();
+
+            //New line to separate results
+            Console.WriteLine();
+
+            //Call the lambda expression syntax
+            UsingLambdaSyntax();
+
+            //Wait for input to prevent instant console closure
+            Console.Read();
+        }
+
+        private static void UsingQuerySyntax()
+        {
+            var files = new DirectoryInfo("c:\\Windows").GetFiles();
+            var query = from item in files
+                        where item.Length > 10000
+                        orderby item.Length, item.Name
+                        select new MyFileInfo
+                        {
+                            Name = item.Name,
+                            Length = item.Length,
+                            CreationTime = item.CreationTime
+                        };
+
+            Console.WriteLine("FileName\tSize\t\tCreation Date");
+            foreach (var item in query)
+            {
+                Console.WriteLine("{0} \t{1} bytes, \t{2}",
+                    item.Name, item.Length, item.CreationTime);
+            }
+        }
+
+        private static void UsingLambdaSyntax()
+        {
+            var files = new DirectoryInfo("c:\\Windows").GetFiles();
+            var query = files.Where(f => f.Length > 10000)
+                .OrderBy(f => f.Length).ThenBy(f => f.Name)
+                .Select(f => new MyFileInfo
+                {
+                    Name = f.Name,
+                    Length = f.Length,
+                    CreationTime = f.CreationTime
+                });
+
+            Console.WriteLine("FileName\tSize\t\tCreation Date");
+            foreach (var item in query)
+            {
+                Console.WriteLine("{0} \t{1} bytes, \t{2}",
+                    item.Name, item.Length, item.CreationTime);
+            }
+        }
+    }
+}
