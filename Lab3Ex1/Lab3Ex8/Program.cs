54504d8177c21694e0531a5402896eaa642f6e88
Steven Kelly <stevenkelly418@gmail.com>
2022-02-09 13:54:09 +0000

    => Added Lambda expression syntax to exercise 8.

--- Lab3Ex1/Lab3Ex8/Program.cs
+++ Lab3Ex1/Lab3Ex8/Program.cs
@@ -10,14 +10,33 @@ namespace Lab3Ex8
     {
         static void Main(string[] args)
         {
+            //Call Query syntax method
+            UsingQuerySyntax();
+
+            //New line to separate results
+            Console.WriteLine();
+
+            //Call the lambda syntax method
+            UsingLambdaSyntax();
+
+            //Wait for input to prevent instant console closure
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Queries a list of names using LINQ Query syntax for those beginning with M and
+        /// orders them alphabetically.
+        /// </summary>
+        private static void UsingQuerySyntax()
+        {
             //Declare string array of names
             string[] names = { "Mary", "Joseph", "Michael", "Sarah", "Margaret", "John" };
 
             //Query the array of names beginning with M and sort alphabetically
             var query = from n in names
-                                        where  n.StartsWith("M")
-                                        orderby (n)
-                                        select (n);
+                where n.StartsWith("M")
+                orderby (n)
+                select (n);
 
             //Iterate through result set returned via the query
             foreach (var name in query)
@@ -25,9 +44,26 @@ namespace Lab3Ex8
                 //Print the element in the set
                 Console.WriteLine(name);
             }
+        }
 
-            //Wait for input to prevent instant console closure
-            Console.ReadLine();
+        /// <summary>
+        /// Queries a list of names using LINQ Lambda syntax for those beginning with M and
+        /// orders them alphabetically.
+        /// </summary>
+        private static void UsingLambdaSyntax()
+        {
+            //Declare string array of names
+            string[] names = { "Mary", "Joseph", "Michael", "Sarah", "Margaret", "John" };
+
+            //Query the array of names beginning with M and sort alphabetically
+            var query = names.Where(n => n.StartsWith("M")).OrderBy(n => n).Select(n => n);
+
+            //Iterate through result set returned via the query
+            foreach (var name in query)
+            {
+                //Print the element in the set
+                Console.WriteLine(name);
+            }
         }
     }
 }
