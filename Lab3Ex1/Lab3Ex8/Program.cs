e63d5e353afb2dce93403d95b51f9486b85d9779
Steven Kelly <stevenkelly418@gmail.com>
2022-02-09 11:58:14 +0000

    => Added Exercise 8 to the repo.

---
+++ Lab3Ex1/Lab3Ex8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3Ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare string array of names
+            string[] names = { "Mary", "Joseph", "Michael", "Sarah", "Margaret", "John" };
+
+            //Query the array of names beginning with M and sort alphabetically
+            var query = from n in names
+                                        where  n.StartsWith("M")
+                                        orderby (n)
+                                        select (n);
+
+            //Iterate through result set returned via the query
+            foreach (var name in query)
+            {
+                //Print the element in the set
+                Console.WriteLine(name);
+            }
+
+            //Wait for input to prevent instant console closure
+            Console.ReadLine();
+        }
+    }
+}
