300d5a8610fb8eb353847f46b1403202b3c7d7f6
Steven Kelly <stevenkelly418@gmail.com>
2022-02-09 13:49:17 +0000

    => Added Lambda expression LINQ query syntax to exercise 9.

--- Lab3Ex1/Lab3Ex9/Program.cs
+++ Lab3Ex1/Lab3Ex9/Program.cs
@@ -11,6 +11,24 @@ namespace Lab3Ex9
     {
         static void Main(string[] args)
         {
+            //Call Query syntax method
+            UsingQuerySyntax();
+
+            //New line to separate results
+            Console.WriteLine();
+
+            //Call the lambda syntax method
+            UsingLambdaSyntax();
+
+            //Wait for input to prevent instant console closure
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Queries list of customers using LINQ query syntax where Dublin is listed as the city. 
+        /// </summary>
+        private static void UsingQuerySyntax()
+        {
             //Get all customers who Dublin listed as city
             var query = from c in GetCustomers()
                 where c.City == "Dublin"
@@ -22,9 +40,27 @@ namespace Lab3Ex9
                 //Print name and city to console
                 Console.WriteLine($"{item.Name} - {item.City}");
             }
+        }
 
-            //Wait for input to prevent instant console closure
-            Console.ReadLine();
+
+        /// <summary>
+        /// Queries list of customers using LINQ lambda syntax where Dublin is listed as the city. 
+        /// </summary>
+        private static void UsingLambdaSyntax()
+        {
+            var results = GetCustomers();
+
+            //Get all customers who Dublin listed as city
+            var query = results
+                .Where(c => c.City == "Dublin")
+                .Select(c=> c);
+
+            //Iterate through result set
+            foreach (var item in query)
+            {
+                //Print name and city to console
+                Console.WriteLine($"{item.Name} - {item.City}");
+            }
         }
 
         /// <summary>
