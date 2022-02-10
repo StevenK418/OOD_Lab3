5e7cc40fa9008b8fe08984ee258202a04122f6dc
Steven Kelly <stevenkelly418@gmail.com>
2022-02-10 08:57:24 +0000

    => Added Lambda syntax methods to Exercise 7.

--- Lab3Ex1/Lab3Ex7/Program.cs
+++ Lab3Ex1/Lab3Ex7/Program.cs
@@ -10,13 +10,32 @@ namespace Lab3Ex7
     {
         static void Main(string[] args)
         {
+            //Call Query syntax method
+            UsingQuerySyntax();
+
+            //New line to separate results
+            Console.WriteLine();
+
+            //Call the lambda syntax method
+            UsingLambdaSyntax();
+
+            //Wait for input to prevent instant console closure
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Queries a list of names using LINQ Query syntax for those beginning with M and
+        /// orders them alphabetically.
+        /// </summary>
+        private static void UsingQuerySyntax()
+        {
             //Declare string array of names
             string[] names = { "Mary", "Joseph", "Michael", "Sarah", "Margaret", "John" };
 
             //Query the array of names and sort alphabetically
             var query = from n in names
-                                        orderby (n)
-                                        select (n);
+                orderby (n)
+                select (n);
 
             //Iterate through result set returned via the query
             foreach (var name in query)
@@ -24,9 +43,26 @@ namespace Lab3Ex7
                 //Print the element in the set
                 Console.WriteLine(name);
             }
+        }
 
-            //Wait for input to prevent instant console closure
-            Console.ReadLine();
+        /// <summary>
+        /// Queries a list of names using LINQ Lambda syntax for those beginning with M and
+        /// orders them alphabetically.
+        /// </summary>
+        private static void UsingLambdaSyntax()
+        {
+            //Declare string array of names
+            string[] names = { "Mary", "Joseph", "Michael", "Sarah", "Margaret", "John" };
+
+            //Query the array of names and sort alphabetically
+            var query = names.OrderBy(n => n).Select(n => n);
+
+            //Iterate through result set returned via the query
+            foreach (var name in query)
+            {
+                //Print the element in the set
+                Console.WriteLine(name);
+            }
         }
     }
 }
